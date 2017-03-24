54fa9991973c61764e11f0b89c6770cc79c950da
keryl <kerylklonye@gmail.com>
2017-03-24 14:56:11 +0300

    modify t file

---
+++ t.py
@@ -0,0 +1,16 @@
+def typeof(x):
+    for x in typeof():
+        if typeof(x) == str:
+            return 'string'
+        elif typeof(x) == number:
+            return 'number'
+        elif typeof(x) == number:
+            return 'number'
+        elif typeof(x) == array:
+            return 'array'
+        elif typeof(x) == dict:
+            return 'dict'
+        elif typeof(x) == unknown:
+            return 'unknown'
+        else:
+            pass
