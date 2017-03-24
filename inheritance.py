b636c42d08b49e8c3e6f7ab23aba253ddeb50564
keryl <kerylklonye@gmail.com>
2017-03-25 01:09:13 +0300

    modify inheritance

---
+++ inheritance.py
@@ -0,0 +1,31 @@
+class Food(object):
+
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+    def get_name(self):
+         self.name = name
+         return name
+    def set_name(self):
+        self.name = name
+        return name
+
+
+  class Fruit(Food):
+
+     def __init__(self, color):
+         self.color = ""
+        return color
+
+     def get_name(self):
+         self.name = name
+         return name
+
+     def set_name(self):
+         self.name = name
+         return name
+
+
+
+
