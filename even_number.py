6bcdb3d84ec0f92d50de30c362f730ad6c9f37d9
keryl <kerylklonye@gmail.com>
2017-03-24 19:20:26 +0300

    even_num should only accept a list

--- even_number.py
+++ even_number.py
@@ -1,5 +1,11 @@
 def even_num(l):
 
+    # first, we will ensure l is of type "list"
+    if type(l) != list:
+        return "argument passed is not a list of numbers"
+
+    # else l is a list so find even numbers in it
+
     enum = []
     for n in l:
         if n % 2 == 0:
--- test_even_number.py
+++ test_even_number.py
@@ -8,5 +8,23 @@ class TestEven_numbers(unittest.TestCase):
         expected_even_numbers = [2, 4, 6, 8]
         self.assertEqual(expected_even_numbers, even_numbers)
 
+    def test_if_argument_is_not_a_list(self):
+
+        # should return error if argument is a boolean
+        result = even_number.even_num(True)
+        self.assertEqual("argument passed is not a list of numbers", result)
+
+        # should return error if argument is a string
+        result = even_number.even_num("test")
+        self.assertEqual("argument passed is not a list of numbers", result)
+
+        # should return error if argument is a number
+        result = even_number.even_num(1234)
+        self.assertEqual("argument passed is not a list of numbers", result)
+
+        # dictionary
+        result = even_number.even_num({})
+        self.assertEqual("argument passed is not a list of numbers", result)
+
 if __name__=="__main__":
     unittest.main()
