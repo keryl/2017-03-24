2e6741003f7dffd6fb691b8e7f665b1475b077db
keryl <kerylklonye@gmail.com>
2017-03-25 18:56:03 +0300

    add test_fruit_inheritance file

--- inheritance.py
+++ inheritance.py
@@ -7,24 +7,17 @@ class Food(object):
     def get_name(self):
          self.name = name
          return name
+
     def set_name(self):
         self.name = name
         return name
 
 
-  class Fruit(Food):
-
-     def __init__(self, color):
-         self.color = ""
+class Fruit(Food):
+    def __init__(self, color):
+        self.color = ""
         return color
 
-     def get_name(self):
-         self.name = name
-         return name
-
-     def set_name(self):
-         self.name = name
-         return name
 
 
 
